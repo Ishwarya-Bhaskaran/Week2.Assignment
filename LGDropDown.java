62690d866c888744a4fa5d27716fd7a6caefe68a
Ishwarya-Bhaskaran <92179044+Ishwarya-Bhaskaran@users.noreply.github.com>
2021-11-15 23:34:20 +0530

    Add files via upload

--- LGDropDown.java
+++ LGDropDown.java
@@ -1,5 +1,6 @@
 package Week2Assignments;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,13 +11,15 @@ import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LGDropDown {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException
 	{
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("http://leafground.com/pages/Dropdown.html");
 		//maximize the window 
 		driver.manage().window().maximize();
+		//wait 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 		//select training program using index
 		WebElement dropdown= driver.findElement(By.id("dropdown1"));
 		//converting webelement to select class 
@@ -35,11 +38,13 @@ public class LGDropDown {
 		Select choose2=new Select(dropdown2);
 		//choose training program using text 
 		choose2.selectByValue("3");
-		//number of dropdown options 
-		//List<WebElement> dropdowncount= driver.findElements(By.className("dropdown"));
-		//Select choose3= new Select((WebElement) driver.findElements(By.className("dropdown")));
-				//int count= ((List<WebElement>) choose3).size();
-			//	System.out.println("The number of options in this dropdown are:"+count);
+		//assign and select dropdown list element
+		Select dropdownlist= new Select(driver.findElement(By.className("dropdown")));
+		//get all options and assign into list 
+		List<WebElement> dropdowncount= dropdownlist.getOptions();
+		//count and assign into int variable
+		  int count= dropdowncount.size();
+			System.out.println("The number of options in this dropdown are:"+count);
 				//sendKeys doubt 
 		//disabled dropdown		
      // WebElement ele= driver.findElement(By.cssSelector("div#contentblock>section>div:nth-of-type(6)>select>option:nth-of-type(3)"));
